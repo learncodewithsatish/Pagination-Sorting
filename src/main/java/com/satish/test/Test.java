08267d048e0a3e45ab381f19ed8cd827c5dfeb02
satish <sk95satish@gmail.com>
2025-07-18 15:01:26 +0530

    Revert "Line 2"
    This reverts commit 8b119ee072c70abd2c1a6bf79ef35de0500443c4.

--- src/main/java/com/satish/test/Test.java
+++ src/main/java/com/satish/test/Test.java
@@ -3,6 +3,5 @@ package com.satish.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Line 2");
     }
 }
