3626a8a23c83e5553c856d33cd5eb96afd8db30e
satish <sk95satish@gmail.com>
2025-07-18 15:11:17 +0530

    Resolved commit

--- src/main/java/com/satish/test/Test.java
+++ src/main/java/com/satish/test/Test.java
@@ -3,7 +3,6 @@ package com.satish.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Line 2");
         System.out.println("Line 3");
     }
 }
